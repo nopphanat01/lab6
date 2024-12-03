a179b79a18fe32ba44e0d871c69e75c59a398c09
nopphanat01 <sspas401@gmail.com>
2024-12-03 14:51:18 +0700

    lab6_2.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int Even_number = 0 ;
+    int Odd_number = 0 ;
+    int N = 1 ;
+    while (N!=0){
+        cout << "Enter an integer: ";
+        cin >> N;
+        if (N!=0){
+            if(N%2==0){
+                Even_number+=1;
+            }
+            else{
+                Odd_number+=1;
+            }   
+        }        
+    }
+    cout << "#Even numbers = "<<Even_number<<"\n";
+    cout << "#Odd numbers = "<<Odd_number<<"\n";
     return 0;
 }
